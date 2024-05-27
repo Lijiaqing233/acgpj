6be4da24a7dfa95ae4ac951e6dbe30524f706e90
ljq <74410648+Lijiaqing233@users.noreply.github.com>
2024-05-27 23:29:57 +0900

    add init code

---
+++ acgpj/Main.cpp
@@ -0,0 +1,9 @@
+#include "Main.h"
+#include "Windows.h"
+#include "iostream"
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, 
+					int nCmdShow)
+{
+	MessageBox(nullptr, "to be continued...", "Acg Engine", MB_OK);
+}
