ee2a65d968818125c906b81ab4cbbab828b6f432
ljq <74410648+Lijiaqing233@users.noreply.github.com>
2024-05-28 13:56:11 +0900

    add main windows

--- acgpj/Main.cpp
+++ acgpj/Main.cpp
@@ -1,9 +1,59 @@
 #include "Main.h"
 #include "Windows.h"
 #include "iostream"
+#include "DirectXMath.h"
+#include "d3dcompiler.h"
+#include "d3d12.h"
+
+int CreateWindowsClass(HINSTANCE hInstance)
+{
+	std::string className = "MyWindowsClass";
+	WNDCLASSEX windowsClass = {0};
+	windowsClass.cbSize = sizeof(WNDCLASSEX);
+	windowsClass.lpszClassName = className.c_str();
+	windowsClass.lpfnWndProc = DefWindowProc;
+	windowsClass.hInstance = hInstance;
+
+	auto registerClass = RegisterClassEx(&windowsClass);
+
+	if(registerClass == 0)
+	{
+		GetLastError();
+		return -1;
+	}
+
+	HWND hwnd = CreateWindowEx(NULL,
+		className.c_str(),
+		"AcgEngine",
+		WS_OVERLAPPEDWINDOW,
+		100,
+		200,
+		800,
+		600,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr);
+
+	if(hwnd == 0)
+	{
+		GetLastError();
+		return -1;
+	}
+	ShowWindow(hwnd, SW_SHOW);
+
+	while (true)
+	{
+
+
+	}
+	return 0;
+
+}
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, 
-					int nCmdShow)
+                    int nCmdShow)
 {
-	MessageBox(nullptr, "to be continued...", "Acg Engine", MB_OK);
+
+	CreateWindowsClass(hInstance);
 }
