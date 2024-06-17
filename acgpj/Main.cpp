69ef187e3f1daefa6a8079d91bf4f78a9777e2d1
ljq <74410648+Lijiaqing233@users.noreply.github.com>
2024-06-17 18:08:23 +0900

    del old file

--- acgpj/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Main.h"
-#include "Windows.h"
-#include "iostream"
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, 
-					int nCmdShow)
-{
-	MessageBox(nullptr, "to be continued...", "Acg Engine", MB_OK);
-}
